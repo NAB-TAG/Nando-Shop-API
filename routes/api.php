7673e132a350d268c0e924aa8898d1b712ee8f9c
Nando Agustin Bravo <bravonandoagustin@gmail.com>
2024-02-02 15:42:27 -0300

    test(test): :construction: Probando el deploy

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/prueba', function () {
-    return ["Probando"];
+    return ["Probando el deploy"];
 });
