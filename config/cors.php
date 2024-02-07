28c1e0658ed0acef6bc5ae0817b7fa9f666800cf
Nando Agustin Bravo <bravonandoagustin@gmail.com>
2024-02-07 18:01:56 -0300

    fix: :construction: Agregue mas paths al cors.php

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'user', 'auth', 'auth/callback', 'cookie'],
 
     'allowed_methods' => ['*'],
 
